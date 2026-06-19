4d383323875df8388f03c9883f7af2330f4f71c1
pociello <asierpociello@gmail.com>
2026-06-19 12:44:30 +0200

    feat: add HomePage layout

---
+++ src/pages/homePage.jsx
@@ -0,0 +1,22 @@
+function HomePage(){
+
+    return (
+        <div className="home-page-container">
+            <header>
+                <p>CVStudio</p>
+                <button>
+                    theme
+                </button>
+            </header>
+
+            <div className="home-info">
+                <h1>Build Your Professional CV in Minutes</h1>
+                <p>A simple tool to build your CV. Add your details, organize sections, and keep everything in one place.</p>
+                <button>CREATE CV</button>
+            </div>
+            
+        </div>
+    )
+}
+
+export default HomePage
