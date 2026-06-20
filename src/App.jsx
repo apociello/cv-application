987fad19e8ca4bba423d8de40fdca807cceb1a5a
pociello <asierpociello@gmail.com>
2026-06-20 13:20:35 +0200

    refactor: Pull header out of HomePage component Like this we just write the header once and avoid repeating it through the different pages.

--- src/App.jsx
+++ src/App.jsx
@@ -1,15 +1,32 @@
-import { useState } from 'react'
-import HomePage from "./pages/homePage"
-import './App.css'
+import { useState } from 'react';
+import HomePage from './pages/homePage';
+import './App.css';
+import sunIcon from './assets/images/sun.svg';
+import moonIcon from './assets/images/moon.svg';
 
 function App() {
   const [darkMode, setDarkMode] = useState(false);
 
+  const icon = darkMode ? moonIcon : sunIcon;
+
+  function toggleDarkMode() {
+    setDarkMode(!darkMode);
+    document.documentElement.className = darkMode ? 'light' : 'dark';
+  }
+
   return (
     <>
-      <HomePage darkMode={darkMode} setDarkMode={setDarkMode}/>
+      <header>
+        <p>CVStudio</p>
+
+        <button onClick={() => toggleDarkMode()}>
+          <img src={icon} width="21" alt="" />
+        </button>
+      </header>
+
+      <HomePage />
     </>
-  )
+  );
 }
 
-export default App
+export default App;
--- src/pages/homePage.jsx
+++ src/pages/homePage.jsx
@@ -1,29 +1,10 @@
-import sunIcon from '../assets/images/sun.svg';
-import moonIcon from '../assets/images/moon.svg';
-
-function HomePage({ darkMode, setDarkMode }) {
-  const icon = darkMode ? moonIcon : sunIcon;
-
-  function toggleDarkMode() {
-    setDarkMode(!darkMode);
-    document.documentElement.className = darkMode ? 'light' : 'dark';
-  }
-
+function HomePage() {
   return (
-    <div className="home-page-container">
-      <header>
-        <p>CVStudio</p>
-        <button onClick={() => toggleDarkMode()}>
-          <img src={icon} width="21" alt="" />
-        </button>
-      </header>
-
-      <main className="home-info">
-        <h1>Build Your Professional CV in Minutes</h1>
-        <p>Fill in your details, we handle the rest.</p>
-        <button>Create CV</button>
-      </main>
-    </div>
+    <main className="home-info">
+      <h1>Build Your Professional CV in Minutes</h1>
+      <p>Fill in your details, we handle the rest.</p>
+      <button>Create CV</button>
+    </main>
   );
 }
 
