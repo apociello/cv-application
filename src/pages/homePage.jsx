37a10f9cfb66069fb8df8e3c3b74c3fcccff8cd6
pociello <asierpociello@gmail.com>
2026-06-20 10:51:00 +0200

    fix: fix toggling error with light/dark icon

--- src/pages/homePage.jsx
+++ src/pages/homePage.jsx
@@ -1,31 +1,30 @@
-import sunIcon from "../assets/images/sun.svg"
-import moonIcon from "../assets/images/moon.svg"
-function HomePage({darkMode, setDarkMode}){
+import sunIcon from '../assets/images/sun.svg';
+import moonIcon from '../assets/images/moon.svg';
 
-    const icon = darkMode ? moonIcon : sunIcon;
+function HomePage({ darkMode, setDarkMode }) {
+  const icon = darkMode ? moonIcon : sunIcon;
 
-    function toggleDarkMode(){
-        setDarkMode(!darkMode);
-        document.documentElement.className = darkMode ? 'dark' : 'light'
-    }
+  function toggleDarkMode() {
+    setDarkMode(!darkMode);
+    document.documentElement.className = darkMode ? 'light' : 'dark';
+  }
 
-    return (
-        <div className="home-page-container">
-            <header>
-                <p>CVStudio</p>
-                <button onClick={() => toggleDarkMode()}>
-                    <img src={icon} width="21" alt="" />
-                </button>
-            </header>
+  return (
+    <div className="home-page-container">
+      <header>
+        <p>CVStudio</p>
+        <button onClick={() => toggleDarkMode()}>
+          <img src={icon} width="21" alt="" />
+        </button>
+      </header>
 
-            <div className="home-info">
-                <h1>Build Your Professional CV in Minutes</h1>
-                <p>Fill in your details, we handle the rest.</p>
-                <button>Create CV</button>
-            </div>
-            
-        </div>
-    )
+      <main className="home-info">
+        <h1>Build Your Professional CV in Minutes</h1>
+        <p>Fill in your details, we handle the rest.</p>
+        <button>Create CV</button>
+      </main>
+    </div>
+  );
 }
 
-export default HomePage
+export default HomePage;
