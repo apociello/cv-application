bfd467c61201e019c918c96051d3c5d403eae2f0
pociello <asierpociello@gmail.com>
2026-06-20 20:00:06 +0200

    feat: create CVPage skeleton layout

--- src/pages/CvPage.jsx
+++ src/pages/CvPage.jsx
@@ -1,15 +1,39 @@
+import './CvPage.css';
+
 function CvPage() {
-    return (
-        <main>
-            <div className="sections">
+  return (
+    <main className="cv-container">
+      <div className="sections">
+        <section className="contact">
+          <h4>CONTACT</h4>
+        </section>
 
-            </div>
+        <section className="languages">
+          <h4>LANGUAGES</h4>
+        </section>
 
-            <div className="preview">
-                
-            </div>
-        </main>
-    )
+        <section className="technical-skills">
+          <h4>TECHNICAL SKILLS</h4>
+        </section>
+
+        <section className="education">
+          <h4>EDUCATION</h4>
+        </section>
+
+        <section className="experience">
+          <h4>EXPEREINCE</h4>
+        </section>
+
+        <section className="other-info">
+          <h4>OTHER INFO</h4>
+        </section>
+      </div>
+
+      <div className="preview">
+        <p>preview</p>
+      </div>
+    </main>
+  );
 }
 
 export default CvPage;
