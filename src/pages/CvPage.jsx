3e37bd6d299853a4b69366814bea7efc8d451ad5
pociello <asierpociello@gmail.com>
2026-06-22 08:46:37 +0200

    feat: create form structure for the preview

--- src/pages/CvPage.jsx
+++ src/pages/CvPage.jsx
@@ -6,17 +6,95 @@ function CvPage() {
       <div className="sections">
         <section className="contact">
           <h4>CONTACT</h4>
+          
+          <div className="form-line">
+            <label htmlFor="first-name">First Name</label>
+            <input id='first-name' type="text" />
+          </div>
+
+          <div className="form-line">
+            <label htmlFor="last-name">Last Name</label>
+            <input id='last-name' type="text" />
+          </div>
+
+          <div className="form-line">
+            <label htmlFor="telephone">Telephone</label>
+            <input id='telephone' type="text" />
+          </div>
+
+          <div className="form-line">
+            <label htmlFor="email">Email</label>
+            <input id='email' type="text" />
+          </div>
+
+          <div className="form-line">
+            <label htmlFor="location">Location</label>
+            <input id='location' type="text" />
+          </div>
+
         </section>
 
         <section className="profile">
           <h4>PROFILE</h4>
+          <textarea name="" id=""></textarea>
         </section>
         <section className="education">
           <h4>EDUCATION</h4>
+
+          <div className="form-line">
+            <label htmlFor="institution">Institution</label>
+            <input id='institution' type="text" />
+          </div>
+
+          <div className="form-line">
+            <label htmlFor="degree">Degree</label>
+            <input id='degree' type="text" />
+          </div>
+
+          <div className="form-line">
+            <label htmlFor="start-year">Start Year</label>
+            <input id='start-year' type="text" />
+          </div>
+
+          <div className="form-line">
+            <label htmlFor="end-year">End Year</label>
+            <input id='end-year' type="text" />
+          </div>
+
+          <div className="form-line">
+            <label htmlFor="description">Description</label>
+            <input id='description' type="text" />
+          </div>
+
         </section>
 
         <section className="experience">
           <h4>EXPEREINCE</h4>
+
+          <div className="form-line">
+            <label htmlFor="company">Company</label>
+            <input id='company' type="text" />
+          </div>
+
+          <div className="form-line">
+            <label htmlFor="position">Position</label>
+            <input id='position' type="text" />
+          </div>
+
+           <div className="form-line">
+            <label htmlFor="start-year">Start Year</label>
+            <input id='start-year' type="text" />
+          </div>
+
+          <div className="form-line">
+            <label htmlFor="end-year">End Year</label>
+            <input id='end-year' type="text" />
+          </div>
+
+           <div className="form-line">
+            <label htmlFor="description">Description</label>
+            <input id='description' type="text" />
+          </div>
         </section>
       </div>
 
@@ -30,8 +108,6 @@ function CvPage() {
           <div className="preview-contact">
             <p>telephone: +1 (617) 555-2847</p>
             <p>email: ethan.carter.dev@gmail.com</p>
-            <p>id: EC-732194</p>
-            <p>birth-date: 11/03/1999</p>
             <p>Boston, USA</p>
           </div>
         </div>
