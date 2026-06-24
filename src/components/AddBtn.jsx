9bbdcc7d7e1ee2519d6d779925c26bcba150e278
pociello <asierpociello@gmail.com>
2026-06-24 11:57:43 +0200

    refactor: create AddBtn component

---
+++ src/components/AddBtn.jsx
@@ -0,0 +1,22 @@
+function AddBtn({ onClick }) {
+  return (
+    <button onClick={onClick}>
+      <svg
+        viewBox="0 0 24 24"
+        xmlns="http://www.w3.org/2000/svg"
+        width="15"
+        height="15"
+        fill="none"
+        stroke="currentColor"
+        strokeWidth="2"
+        strokeLinecap="round"
+        strokeLinejoin="round"
+      >
+        <line x1="12" x2="12" y1="19" y2="5" />
+        <line x1="5" x2="19" y1="12" y2="12" />
+      </svg>
+    </button>
+  );
+}
+
+export default AddBtn;
--- src/pages/CvPage.jsx
+++ src/pages/CvPage.jsx
@@ -5,6 +5,7 @@ import Contact from '../components/Contact';
 import Profile from '../components/Profile';
 import Study from '../components/Study';
 import Job from '../components/Job';
+import AddBtn from '../components/AddBtn';
 import './CvPage.css';
 
 function CvPage() {
@@ -44,38 +45,21 @@ function CvPage() {
             />
           ))}
 
-          <div className="add-div">
-            <button
-              onClick={() =>
-                setEducation([
-                  ...education,
-                  {
-                    id: crypto.randomUUID(),
-                    institution: '',
-                    startYear: '',
-                    endYear: '',
-                    degree: '',
-                    description: '',
-                  },
-                ])
-              }
-            >
-              <svg
-                viewBox="0 0 24 24"
-                xmlns="http://www.w3.org/2000/svg"
-                width="15"
-                height="15"
-                fill="none"
-                stroke="currentColor"
-                strokeWidth="2"
-                strokeLinecap="round"
-                strokeLinejoin="round"
-              >
-                <line x1="12" x2="12" y1="19" y2="5" />
-                <line x1="5" x2="19" y1="12" y2="12" />
-              </svg>
-            </button>
-          </div>
+          <AddBtn
+            onClick={() =>
+              setEducation([
+                ...education,
+                {
+                  id: crypto.randomUUID(),
+                  institution: '',
+                  startYear: '',
+                  endYear: '',
+                  degree: '',
+                  description: '',
+                },
+              ])
+            }
+          />
         </section>
 
         <section className="experience">
@@ -89,38 +73,21 @@ function CvPage() {
             />
           ))}
 
-          <div className="add-div">
-            <button
-              onClick={() =>
-                setExperience([
-                  ...experience,
-                  {
-                    id: crypto.randomUUID(),
-                    company: '',
-                    startYear: '',
-                    endYear: '',
-                    position: '',
-                    description: '',
-                  },
-                ])
-              }
-            >
-              <svg
-                viewBox="0 0 24 24"
-                xmlns="http://www.w3.org/2000/svg"
-                width="15"
-                height="15"
-                fill="none"
-                stroke="currentColor"
-                strokeWidth="2"
-                strokeLinecap="round"
-                strokeLinejoin="round"
-              >
-                <line x1="12" x2="12" y1="19" y2="5" />
-                <line x1="5" x2="19" y1="12" y2="12" />
-              </svg>
-            </button>
-          </div>
+          <AddBtn
+            onClick={() =>
+              setExperience([
+                ...experience,
+                {
+                  id: crypto.randomUUID(),
+                  company: '',
+                  startYear: '',
+                  endYear: '',
+                  position: '',
+                  description: '',
+                },
+              ])
+            }
+          />
         </section>
       </div>
 
